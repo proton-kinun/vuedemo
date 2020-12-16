6de663062f4f730aa8dcb1bb2f24b622fbb41248
Proton Kinun <proton-kinun@gmail.com>
2020-12-16 18:55:51 +0100

    opraveny header, pridane stranky

---
+++ src/views/PageJoin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-join">
+    <h1>Tu bude obsah stránky registrácie</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageJoin'
+}
+</script>
+
+<style lang="scss" scoped>
+.page-join {
+  min-height: 100%;
+
+  display: grid;
+  place-items: center center;
+
+  h1 {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+}
+</style>
