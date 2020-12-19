7feb015eeaeac6d42f90eea3a0785a0e028c663a
Proton Kinun <proton-kinun@gmail.com>
2020-12-19 12:35:40 +0100

    signup test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import auth0 from 'auth0-js'
 import router from '@/plugins/router'
 import vuetify from '@/plugins/vuetify'
 import Layout from '@/Layout'
+
+Vue.prototype.$auth = new auth0.WebAuth({
+  domain:       'proton-kinun.eu.auth0.com',
+  clientID:     'Tb9FwmuvJTZY3haYweOlzBCEwZfQ242R'
+})
 
 new Vue({
   router,
--- src/views/PageJoin.vue
+++ src/views/PageJoin.vue
@@ -1,8 +1,10 @@
 <template>
   <v-container class="fill-height">
-    <v-row>
-      <v-col>
-        <h1 class="text-center">Tu bude registračný formulár</h1>
+    <v-row justify="center">
+      <v-col cols="2">
+        <v-btn @click.stop="register" elevation="2">
+          Register vladol
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -10,6 +12,20 @@
 
 <script>
 export default {
-  name: 'PageJoin'
+  name: 'PageJoin',
+
+  methods: {
+    register () {
+      this.$auth.signup({
+        connection: 'Username-Password-Authentication',
+        email: 'vladislav.ladicky@gmail.com',
+        password: 'vladol',
+        user_metadata: { first: 'Vladislav', last: 'Ladický' }
+      }, function (err) {
+        if (err) return console.log(err);
+        return console.log('success signup without login!')
+      })
+    }
+  }
 }
 </script>
